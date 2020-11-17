033017d6d45957dee34df4da819695b522bdea0b
Your Name <you@example.com>
2020-11-16 18:49:48 -0600

    database

---
+++ Client/CreateDatabase.php
@@ -0,0 +1,51 @@
+<?php include "db.php"; ?>
+
+<?php 
+global $connection;
+
+//MANAGER TABLE
+echo "Manager:<br/>";
+$query = "DROP TABLE IF EXISTS Manager";
+$query = mysqli_query($connection, $query);
+checkQuery($query);
+
+$query = "CREATE TABLE `ttowncards`.`Manager` 
+( `ManagerID` INT NOT NULL AUTO_INCREMENT , 
+`ManagerName` TEXT NOT NULL , `ManagerPhone` 
+TEXT NOT NULL , `ManagerEmail` TEXT NOT NULL , 
+`ManagerAddress` TEXT NOT NULL , PRIMARY KEY 
+(`ManagerID`)) ENGINE = InnoDB;";
+$query = mysqli_query($connection, $query);
+checkQuery($query);
+
+
+//EMPLOYEE TABLE
+echo "Employee:<br/>";
+$query = "DROP TABLE IF EXISTS Employee";
+$query = mysqli_query($connection, $query);
+checkQuery($query);
+
+$query = "CREATE TABLE `ttowncards`.`Employee` 
+( `EmployeeID` INT NOT NULL AUTO_INCREMENT , 
+`EmployeeName` TEXT NOT NULL , `EmployeePhone` TEXT NOT NULL , 
+`EmployeeAddress` TEXT NOT NULL , `ManagerID` INT NOT NULL , 
+PRIMARY KEY (`EmployeeID`)) ENGINE = InnoDB;";
+$query = mysqli_query($connection, $query);
+checkQuery($query);
+
+
+
+
+function checkQuery($query)
+{
+    if($query)
+    {
+        echo "query was successful <br/>";
+    }
+    else
+    {
+        echo "query failed <br/>";
+    }
+};
+    
+    ?>
